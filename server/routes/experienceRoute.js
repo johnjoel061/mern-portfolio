e3463fd57c15d7aa0dcd9a5f478fe0d75b70d9ec
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-04-20 18:37:46 +0800

    ...

--- server/routes/experienceRoute.js
+++ server/routes/experienceRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const experienceController = require("../controllers/experienceController");
 
-router.post("/experience/add", experienceController.addSkill);
-router.get("/experience/all", experienceController.getAllSkill);
-router.delete("/experience/:id", experienceController.deleteSkill);
-router.put("/experience/update/:id", experienceController.updateSkill);
+router.post("/experience/add", experienceController.addExperience);
+router.get("/experience/all", experienceController.getAllExperience);
+router.delete("/experience/:id", experienceController.deleteExperience);
+router.put("/experience/update/:id", experienceController.updateExperience);
 
 module.exports = router;
 
